3083db81232941fefb787916099d0d7766cb6f99
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2023-08-12 05:25:17 +0530

    taiwlind not working issue

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,8 +4,7 @@ const config: Config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
+    "./Components/**/*.{js,ts,jsx,tsx}",
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
