5ade932ea5ac72701a8488e68e5757abd4a7583c
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2023-08-13 01:21:41 +0530

    bottom of card, price, popular,

--- Components/Card.tsx
+++ Components/Card.tsx
@@ -1,35 +1,42 @@
-'use client'
+"use client";
 import React, { useState } from "react";
-
-import house from "../img/house.jpg";
+import { BsStars } from "react-icons/bs";
+import { BiBed, BiBath } from "react-icons/bi";
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 
+import { TbRotateRectangle } from "react-icons/tb";
 
-
-const Card = (data : any) => {
+const Card = (data: any) => {
   const [isLiked, setIsLiked] = useState<boolean>(false);
   return (
-    <div className="max-w-xs rounded-md overflow-hidden shadow-lg ">
-      
-      <img className="w-full" src={house.src} alt="Sunset in the mountains" />
+    <div className="w-96 rounded-md overflow-hidden shadow-lg ">
+      <img
+        className="w-full"
+        src={data.img.src}
+        alt="Sunset in the mountains"
+      />
       <a
-          className="absolute -mt-5  w-10 h-10 bg-blue-dark flex items-center justify-center text-center no-underline rounded-full text-white hover:bg-blue-darker"
-          href="#"
-        >
-          <button className="text-white  rounded-es-md rounded-e-md bg-violet-800">Popular</button>
-        </a>
+        className="absolute -mt-5  w-10 h-10 bg-blue-dark flex items-center justify-center text-center no-underline rounded-full text-white hover:bg-blue-darker"
+        href="#"
+      >
+        <button className="text-white  rounded-es-md rounded-e-md bg-violet-800 p-0.5">
+          <div className="flex justify-items-center justify-center">
+            <BsStars /> Popular
+          </div>
+        </button>
+      </a>
       <div className="px-6 py-4">
         <div className="flex justify-between">
-       
-          <div className="font-bold text-xl mb-2">${data.price}/month</div>
-            <button className="border-solid border-2 inline-block items-center rounded-full px-2 py-2 text-sm font-semibold text-gray-700" onClick={
-                () => setIsLiked(!isLiked)
-            }>
-            {isLiked ?
-                <AiFillHeart className="text-red-500 text-4xl" />
-            :
-                <AiOutlineHeart className=" text-4xl" />
-            }
+          <div className="font-bold text-xl mb-2"><span className="text-violet-800 text-3xl">${data.price}</span><span className="text-gray-500 font-normal">/month</span></div>
+          <button
+            className="border-solid border-2 inline-block items-center rounded-full px-2 py-2 text-sm font-semibold text-gray-700"
+            onClick={() => setIsLiked(!isLiked)}
+          >
+            {isLiked ? (
+              <AiFillHeart className="text-red-500 text-4xl" />
+            ) : (
+              <AiOutlineHeart className=" text-4xl" />
+            )}
           </button>
         </div>
         <div className="font-bold text-xl mb-2">{data.title}</div>
@@ -38,14 +45,21 @@ const Card = (data : any) => {
 
       <div className="px-6 pt-4 pb-2">
         <hr className="pt-5 border-gray-400" />
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-          {data.beds} beds
+        <span className="inline-block  rounded-full px-2 py-1 text-sm font-semibold mr-1 mb-1 ">
+          <div className="justify-center justify-items-center items-between grid grid-flow-col">
+            <BiBed className="text-xl" /> {data.beds} beds
+          </div>
         </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-          {data.bathrooms} bathrooms
+        <span className="inline-block  rounded-full px-2 py-1 text-sm font-semibold mr-1 mb-1">
+          <div className="justify-between justify-items-between items-between grid grid-flow-col">
+            <BiBath className="text-xl"/> {data.bathrooms} bathrooms
+          </div>
         </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-          {data.sqft} sqft
+        <span className="inline-block  rounded-full px-2 py-1 text-sm font-semibold mr-1 mb-1">
+          <div className="justify-center justify-items-center items-between grid grid-flow-col">
+            <TbRotateRectangle className="text-xl"/> {data.sqft} m
+            <sup className="text-xs align-top">2</sup>
+          </div>
         </span>
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ const additionalData = [
     img: house, // Use appropriate image source
     beds: 2,
     bathrooms: 2,
-    sqft: "1100 sqft",
+    sqft: "6x7.5",
   },
   {
     title: "Urban Loft",
@@ -21,7 +21,7 @@ const additionalData = [
     img: house, // Use appropriate image source
     beds: 1,
     bathrooms: 1,
-    sqft: "900 sqft",
+    sqft: "6x7.5",
   },
   {
     title: "Rustic Cabin",
@@ -31,7 +31,7 @@ const additionalData = [
     img: house, // Use appropriate image source
     beds: 2,
     bathrooms: 1,
-    sqft: "700 sqft",
+    sqft: "6x7.5",
   },
   {
     title: "Lakefront Haven",
@@ -41,7 +41,7 @@ const additionalData = [
     img: house, // Use appropriate image source
     beds: 3,
     bathrooms: 2,
-    sqft: "1800 sqft",
+    sqft: "6x7.5",
   },
   {
     title: "Country Estate",
@@ -51,7 +51,7 @@ const additionalData = [
     img: house, // Use appropriate image source
     beds: 4,
     bathrooms: 3,
-    sqft: "4000 sqft",
+    sqft: "6x7.5",
   },
   {
     title: "Country Estate",
@@ -61,9 +61,8 @@ const additionalData = [
     img: house, // Use appropriate image source
     beds: 4,
     bathrooms: 3,
-    sqft: "4000 sqft",
+    sqft: "6x7.5",
   },
-  // Add more data objects here...
 ];
 
 export default function Home() {
@@ -80,8 +79,8 @@ export default function Home() {
       </div>
       <div className="divide-x  flex mx-auto flex-wrap  py-4  rounded-xl shadow-md justify-items-center justify-center   bg-white">
         <div className="px-10 ">
-          <h1>Location</h1>
-          <select className=" rounded-lg text-sm  border-none focus:ring-0 ">
+          <h1 className="text-gray-400">Location</h1>
+          <select className="-ml-3 font-bold rounded-lg text-sm  border-none focus:ring-0 ">
             <option value="new york">New York, USA</option>
             <option value="chicago">Chicago, </option>
             <option value="san francisco">San Francisco, </option>
@@ -89,23 +88,23 @@ export default function Home() {
         </div>
 
         <div className=" px-10 justify-items-center">
-          <h1>When</h1>
-          <h1 className=" flex flex-row justify-items-center items-center text-center gap-2">
-            Select Move-in Date{" "}
-            <FaCalendar className="text-center text-indigo-500" />{" "}
-          </h1>
+          <h1 className="text-gray-400">When</h1>
+          <input type="date" className=" -ml-3 font-bold rounded-lg text-sm border-none focus:ring-0" id="birthday"  name="birthday"/>
         </div>
-        <div className="px-10">
-          <h1>Price</h1>
-          <select className="rounded-lg text-sm  border-none focus:ring-0">
-            <option value="500-2,500"> $500-$2,500 </option>
-            <option value="2500-3,500"> $2500-$3,500 </option>
-            <option value="3500-5,500"> $3500-$5,500 </option>
+        <div className="px-10 grid grid--2 ">
+          <h1 className="text-gray-400">Price</h1>
+          <div className="w-full -ml-3">
+           <select className="font-bold rounded-lg text-sm    border-none focus:ring-0">
+            <option className="text-left" value="500-2,500"> $500-$2,500 </option>
+            <option className="text-left" value="2500-3,500"> $2500-$3,500 </option>
+            <option className="text-left" value="3500-5,500"> $3500-$5,500 </option>
           </select>
+          </div>
+        
         </div>
         <div className="px-10 grid grid-row">
-          <h1>Property Type</h1>
-          <select className="rounded-lg text-sm  border-none focus:ring-0 ">
+          <h1 className="text-gray-400">Property Type</h1>
+          <select className="font-bold -ml-3 rounded-lg text-sm  border-none focus:ring-0 ">
             <option value="houses"> Houses </option>
             <option value="apartment"> Apartment </option>
             <option value="condo"> Condo </option>
@@ -118,7 +117,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="flex mx-auto flex-wrap  gap-8  pt-10 justify-items-center justify-center">
+      <div className="flex mx-auto flex-wrap  gap-6   py-10 lg:justify-items-between sm:justify-items-center md:justify-items-center lg:justify-between sm:justify-center md:justify-center">
         {additionalData.map((data) => (
           <Card {...data} />
         ))}
